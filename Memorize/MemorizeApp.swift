04dd3063375dcce7e485bd7a6e8a033e02985f75
Anshu Vij <anshu.vij@sustlabs.com>
2024-08-22 23:58:10 +0530

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anshu Vij on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
